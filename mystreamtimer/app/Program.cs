452fda1931c16ffb9c7cfc8e9ce413a4d19cca88
Dara Oladapo <daraoaldapo@outlook.com>
2020-04-26 18:49:11 +0100

    installer and test success

---
+++ mystreamtimer/app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace mystreamtimer.extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          ExecuteCommand(args[0]);
+        }
+        public static  void ExecuteCommand(string Command)
+    {
+        ProcessStartInfo ProcessInfo;
+        Process Process;
+
+        // ProcessInfo = new ProcessStartInfo("cmd.exe", "/K " + Command);
+        ProcessInfo = new ProcessStartInfo(Command);
+        ProcessInfo.CreateNoWindow = true;
+        ProcessInfo.UseShellExecute = true;
+        Process = Process.Start(ProcessInfo);
+    }
+    }
+}
